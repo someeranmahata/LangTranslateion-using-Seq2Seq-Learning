82b2f1a9576098fa329641449304335ee8a319f5
someeranmahata <someeranmahata@gmail.com>
2026-09-21 20:23:03 +0530

    using dataset iitb-eng-hindi

--- Seq2SeqLearning.py
+++ Seq2SeqLearning.py
@@ -1,6 +1,11 @@
 import torch 
 import torch.nn as nn
+from datasets import load_dataset
 
+
+#-------------DATA----------------------------------------
+df = load_dataset("cfilt/iitb-english-hindi")
+print(df.shape, type(df))
 
 #-------------DEVICE------------------------------------------------
 
