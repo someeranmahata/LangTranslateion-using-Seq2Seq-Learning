872893804bfbd567e5a8ebade39215b6046a1075
someeranmahata <someeranmahata@gmail.com>
2026-09-22 09:49:50 +0530

    applied padding and Designed Encoder part

--- Seq2SeqLearning.py
+++ Seq2SeqLearning.py
@@ -3,10 +3,11 @@ import torch.nn as nn
 from torchtext.data import Field
 from datasets import load_dataset
 from collections import Counter
+from torch.nn.utils.rnn import pad_sequence
 
 #-------------DATA----------------------------------------
 dataset = load_dataset("cfilt/iitb-english-hindi")
-print(dataset.shape, type(dataset))
+# print(dataset.shape, type(dataset))
 
 #print(dataset['train'].shape, dataset['train'][0], type(dataset['train'][0]), type(dataset['train'][0]))
 small_train = dataset["train"].select(range(10000))
@@ -40,9 +41,6 @@ def convert():
     for word in counter:
         eng_vocab[word] = len(eng_vocab)
 
-convert()
-print(len(eng_vocab), len(hindi_vocab))
-
 def sent_hindi(sentence):
     tokens = sentence.split()
 
@@ -69,14 +67,47 @@ def sent_english(sentence):
     eng_ids.append(eng_vocab["<EOS>"])
     return eng_ids
 
+def padding_tensor():
+    eng_tensor = [torch.tensor(item) for item in english_sent]
+    hindi_tensor = [torch.tensor(item) for item in hindi_sent]
+
+    eng_padded = pad_sequence(
+        eng_tensor,
+        batch_first=True,
+        padding_value=eng_vocab["<PAD>"]
+    )
+
+    hin_padded = pad_sequence(
+        hindi_tensor,
+        batch_first=True,
+        padding_value=hindi_vocab["<PAD>"]
+    )
+    return eng_padded, hin_padded
+
+convert()
+print(len(eng_vocab), len(hindi_vocab))
+
 
 # print(sent_english("Give your application an accessibility workout"))
 # print(sent_hindi("अपने अनुप्रयोग को पहुंचनीयता व्यायाम का लाभ दें"))
 
-engilsh_sent = [sent_english(i) for i in english]
+english_sent = [sent_english(i) for i in english]
 hindi_sent = [sent_hindi(i) for i in hindi]
 
+print(english_sent[:10], hindi_sent[:10])
 
+maxEnglish = max(len(item) for item in english_sent)
+maxHindi = max(len(item) for item in hindi_sent)
+
+print(maxEnglish, maxHindi)
+
+english_sent, hindi_sent = padding_tensor()
+# for i in range(10):
+#     print(english_sent[i], hindi_sent[i])
+    
+
+    
+    
 #-------------DEVICE------------------------------------------------
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -84,11 +115,23 @@ print(device)
 
 #---------------MODEL BUILDING------------
 class Encoder(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, input_size, hidden_size, num_layers, embedding_size, dp):
+        super(Encoder, self).__init__()
+        
+        self.hidden = hidden_size
+        self.num_layers = num_layers
+        self.embedding = nn.Embedding(input_size, embedding_dim=embedding_size)
+        
+        self.dropout = nn.Dropout(dp)
+        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers=num_layers, dropout=dp, bidirectional=True)
     
-    def forward(self):
-        pass
+    def forward(self, x):
+        
+        embedding=self.dropout(self.embedding(x))
+        output, (hidden, cell) = self.rnn(embedding)
+        
+        return hidden, cell
+        
     
 class Decoder(nn.Module):
     def __init__(self):
